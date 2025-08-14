46bf5ddd266b3bc5ba75e9ebbae31f57453f23b0
richaworth <richaworth@gmail.com>
2025-08-14 10:57:10 +0000

    per tooth tweak

--- tf3_train_per_tooth.py
+++ tf3_train_per_tooth.py
@@ -8,10 +8,12 @@ from monai.losses.dice import DiceCELoss
 from monai.data.utils import decollate_batch 
 from monai.data.dataset import PersistentDataset, Dataset
 from monai.data.dataloader import DataLoader
-from monai.inferers.utils import sliding_window_inference
+from monai.inferers import SimpleInferer
 from monai.metrics.meandice import DiceMetric
 from monai.metrics.hausdorff_distance import HausdorffDistanceMetric
-from monai.networks.nets.unetr import UNETR
+from monai.networks.nets.unet import UNet
+# from monai.networks.nets.unetr import UNETR
+from monai.inferers.utils import sliding_window_inference
 
 from monai.transforms import (
     AsDiscrete, 
@@ -25,7 +27,7 @@ from monai.transforms import (
     Orientationd, 
     SpatialPadd,
     RandAffined, 
-    RandSpatialCropd, 
+    CenterSpatialCropd, 
     RandFlipd,
     RandGaussianNoised,
     RandGaussianSharpend,
@@ -43,10 +45,9 @@ from tqdm import tqdm
 
 import torch
 
-ROI_SIZE = (80, 80, 80)
-AMP = True
+ROI_SIZE = (80, 80, 80)  # 0.3mm^3 voxels ~ 25mm high; 15mm wide, 15mm deep (https://www.cdhp.org/what-is-the-average-size-of-each-tooth/)
+AMP = False
 DEVICE = "cuda:0"
-
            
 def train_model(ld_train: list[dict], 
                 ld_val: list[dict], 
@@ -65,25 +66,31 @@ def train_model(ld_train: list[dict],
 
     set_determinism(deterministic_training_seed)
 
+    train_cases_per_epoch = 600
+    val_cases_per_epoch = 100
+
     train_transforms = Compose(list_train_trans)
     val_transforms = Compose(lst_val_trans)
    
-    train_ds = PersistentDataset(data=ld_train, transform=train_transforms, cache_dir=path_output_dir / f"cache_train_{model_name}",)
+    train_ds = PersistentDataset(data=ld_train, transform=train_transforms, cache_dir=path_output_dir / f"cache_train_{model_name}")
     val_ds = PersistentDataset(data=ld_val, transform=val_transforms, cache_dir=path_output_dir / f"cache_val_{model_name}")
 
-    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=n_workers)
-    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=n_workers)
+    trs = torch.utils.data.RandomSampler(train_ds, replacement=True, num_samples=train_cases_per_epoch)
+    vrs = torch.utils.data.RandomSampler(val_ds, replacement=True, num_samples=val_cases_per_epoch)
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=False, num_workers=n_workers, sampler=trs)
+    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=n_workers, sampler=vrs)
 
     device = torch.device(DEVICE)
     model.to(device)
 
     max_epochs = 400
-    val_interval = 20
-    checkpoint_interval = 20 # Save a checkpoint of the training in case restarting is required (temporary).
+    val_interval = 5
+    checkpoint_interval = 5 # Save a checkpoint of the training in case restarting is required (temporary).
     major_checkpoint_interval = 100 # Save a permanent copy of the current training at major intervals.
     no_improvement_threshold = 5 # If no improvement in the metric within N validation cycles, stop training.
     
-    loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+    loss_function = DiceCELoss(to_onehot_y=True, softmax=True, include_background=False)
     torch.backends.cudnn.benchmark = True
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
 
@@ -99,14 +106,13 @@ def train_model(ld_train: list[dict],
         path_opt_ckpt_previous = None
 
     lr_epoch = -1 if checkpoint_epoch is None else checkpoint_epoch -1
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs, last_epoch=lr_epoch)
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95, last_epoch=lr_epoch)
 
     scaler = torch.GradScaler("cuda") if AMP else None
 
     post_label_trans = Compose([AsDiscrete(to_onehot=n_labels)])
     post_trans = Compose([AsDiscrete(argmax=True, to_onehot=n_labels)])
     dice_metric = DiceMetric(include_background=False, reduction="mean", num_classes=n_labels)
-    hd95_metric = HausdorffDistanceMetric(percentile=95)
     
     best_dsc = -1
     best_dsc_epoch = -1
@@ -128,9 +134,9 @@ def train_model(ld_train: list[dict],
         model.train()
         epoch_loss = 0
         step = 0
-
+        
         for batch_data in tqdm(train_loader):
-            step += 1
+            step+=1
             inputs, labels = (batch_data["inputs"].to(device), batch_data["label"].to(device))
             optimizer.zero_grad()
 
@@ -148,7 +154,7 @@ def train_model(ld_train: list[dict],
                 optimizer.step()
 
             epoch_loss += loss.item()
-            logging.debug(f"{step}/{len(train_ds) // train_loader.batch_size} train_loss: {loss.item():.4f}")
+            logging.debug(f"{step}/{len(train_ds) // train_loader.batch_size} train_loss: {loss.item():.4f}")   
 
         lr_scheduler.step()
         epoch_loss /= step
@@ -184,18 +190,16 @@ def train_model(ld_train: list[dict],
                     
                     if AMP:
                         with torch.autocast("cuda"):
-                            val_outputs = sliding_window_inference(val_inputs, ROI_SIZE, 4, model, 0.75)
+                            val_outputs = model(val_inputs)
                     else:
-                        val_outputs = sliding_window_inference(val_inputs, ROI_SIZE, 4, model, 0.75)
+                        val_outputs = model(val_inputs)
 
                     val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
                     val_labels = [post_label_trans(i) for i in decollate_batch(val_labels)]
                     dice_metric(y_pred=val_outputs, y=val_labels)
-                    hd95_metric(y_pred=val_outputs, y=val_labels)
 
                 dsc = dice_metric.aggregate().item()
-                hd95 = hd95_metric.aggregate().item()
-                metric_values.append((dsc, hd95))
+                metric_values.append((dsc))
 
                 if dsc > best_dsc:
                     best_dsc = dsc
@@ -224,7 +228,6 @@ def train_model(ld_train: list[dict],
                 logging.info(
                     f"current epoch: {epoch + 1} current"
                     f" mean dice: {dsc:.4f}"
-                    f" mean hd95: {hd95:.4f}"
                     f" best mean dice: {best_dsc:.4f} "
                     f"at epoch: {best_dsc_epoch}"
                 )
@@ -267,9 +270,9 @@ def test_model(ld_test: list[dict],
                     
             if AMP:
                 with torch.autocast("cuda"):
-                    test_outputs = sliding_window_inference(test_inputs, ROI_SIZE, 4, model, 0.75)
+                    test_outputs = model(test_inputs)
             else:
-                test_outputs = sliding_window_inference(test_inputs, ROI_SIZE, 4, model, 0.75)
+                test_outputs = model(test_inputs)
 
             test_outputs = [post_trans(i) for i in decollate_batch(test_outputs)]
             
@@ -311,8 +314,8 @@ def main(path_output_dir: Path = Path("C:/data/tf3_per_tooth_output/")):
         EnsureChannelFirstd(keys=["image", "localiser", "label"]),
         Orientationd(keys=["image", "localiser", "label"], axcodes="RAS"),
         ResampleToMatchd(["localiser", "label"], "image", mode=("bilinear", "nearest"), padding_mode="zeros"),
-        ScaleIntensityRanged("image", a_min=-1000, a_max=2000, b_min=0.0, b_max=1.0, clip=True),
-        ScaleIntensityRanged("image", a_min=-50, a_max=0, b_min=0.0, b_max=1.0, clip=True),
+        ScaleIntensityRanged("image", a_min=0, a_max=3000, b_min=0.0, b_max=1.0, clip=True),
+        ScaleIntensityRanged("localiser", a_min=-50, a_max=0, b_min=0.0, b_max=1.0, clip=True),
         SpatialPadd(keys=["image", "localiser", "label"], spatial_size=ROI_SIZE),
         # SaveImaged(keys=["image"], output_postfix="init_image"),  # Comment out unless testing.
         # SaveImaged(keys=["localiser"], output_postfix="init_local"),  # Comment out unless testing.
@@ -320,12 +323,12 @@ def main(path_output_dir: Path = Path("C:/data/tf3_per_tooth_output/")):
     ]
 
     train_only_transforms = [
-        RandAffined(keys=["localiser"], mode=("bilinear"), prob=0.9, translate_range=(10, 10, 10)),  # Add some jitter to the localiser centre
-        RandAffined(keys=["image", "localiser", "label"], mode=("bilinear", "bilinear", "nearest"), prob=0.9, 
-                    rotate_range=(np.pi/15, np.pi/15, np.pi/60), scale_range=(0.1, 0.1, 0.1)),
-        RandSpatialCropd(keys=["image", "localiser", "label"], roi_size=ROI_SIZE),            
+        RandAffined(keys=["image", "localiser", "label"], mode=("bilinear", "nearest", "nearest"), prob=0.9, 
+                    rotate_range=(np.pi/15, np.pi/15, np.pi/60), scale_range=(0.1, 0.1, 0.1), padding_mode="border",
+                    translate_range=(10, 10, 10)),
+        CenterSpatialCropd(keys=["image", "localiser", "label"], roi_size=ROI_SIZE),            
+        RandFlipd(keys=["image", "localiser", "label"], prob=0.5, spatial_axis=2),
         RandShiftIntensityd(keys=["image"], offsets=0.10, prob=0.90),
-        RandFlipd(keys=["image"], prob=0.5, spatial_axis=2),
         RandGaussianNoised(keys=["image"], prob=0.5),
         RandGaussianSmoothd(keys=["image"], prob=0.5),
         RandGaussianSharpend(keys=["image"], prob=0.5),
@@ -335,6 +338,7 @@ def main(path_output_dir: Path = Path("C:/data/tf3_per_tooth_output/")):
     ]
 
     test_val_only_transforms = [
+        CenterSpatialCropd(keys=["image", "localiser", "label"], roi_size=ROI_SIZE),            
         ConcatItemsd(keys=["image", "localiser"], name="inputs"),
         DeleteItemsd(keys=["image", "localiser"]),
         EnsureTyped(keys="inputs", dtype=np.float32)
@@ -346,22 +350,30 @@ def main(path_output_dir: Path = Path("C:/data/tf3_per_tooth_output/")):
     # TODO: Convert to dict transforms; convert onehot to real labels again (potentially outside this transform - can use fuction from tf3_preprocess_images.py).
     postprocessing_transforms = [
         AsDiscrete(argmax=True),
-        SaveImage(output_dir=path_output_dir / "test_segmentations")
+        SaveImage(output_dir=path_output_dir / "test_segmentations", separate_folder=False)
     ]
-    
-    model = UNETR(
+
+    model = UNet(
+        spatial_dims=len(ROI_SIZE),
         in_channels=2,
         out_channels=n_labels,
-        img_size=ROI_SIZE,
-        feature_size=16,
-        hidden_size=768,
-        mlp_dim=3072,
-        num_heads=12,
-        proj_type="perceptron",
-        norm_name="instance",
-        res_block=True,
-        dropout_rate=0.0,
+        channels=(4, 8, 16, 32, 64),
+        strides=(2, 2, 2, 2),
     )
+    
+    # model = UNETR(
+    #     in_channels=2,
+    #     out_channels=n_labels,
+    #     img_size=ROI_SIZE,
+    #     feature_size=16,
+    #     hidden_size=768,
+    #     mlp_dim=3072,
+    #     num_heads=12,
+    #     proj_type="perceptron",
+    #     norm_name="instance",
+    #     res_block=True,
+    #     dropout_rate=0.00,
+    # )
 
     path_tooth_centre_sd = path_data_dir / "labels_tooth_centre_sd"
 
@@ -381,14 +393,14 @@ def main(path_output_dir: Path = Path("C:/data/tf3_per_tooth_output/")):
 
                 ld.append(d)
     
-    mdoel_name = "per_tooth_unetr_diceceloss"
+    model_name = "per_tooth_unetr_diceceloss_600_100"
 
-    train_model(ld_train, ld_val, path_output_dir, model, mdoel_name, train_transforms, test_val_transforms, n_labels,
-                deterministic_training_seed=deterministic_seed, n_workers=4, batch_size=8, checkpoint_epoch=20,
-                path_checkpoint_model=path_output_dir / "checkpoint_per_tooth_unetr_diceceloss_epoch_20.pkl")
+    train_model(ld_train, ld_val, path_output_dir, model, model_name, train_transforms, test_val_transforms, n_labels,
+                deterministic_training_seed=deterministic_seed, n_workers=4, batch_size=25)
     
-    test_model(ld_test, path_output_dir / f"{mdoel_name}_best_metric.pkl", model, test_val_transforms, postprocessing_transforms, 
-               n_labels, n_workers=4, batch_size=1)
+    test_model(ld_test, path_output_dir / "per_tooth_unetr_diceceloss_600_100_best_metric_epoch_400.pkl", model, 
+               test_val_transforms, postprocessing_transforms, 
+               n_labels, n_workers=4, batch_size=8)
 
 
 
